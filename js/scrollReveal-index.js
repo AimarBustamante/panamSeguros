65f25dc3c5646ad64e44eb7daa1c552db9fd54e1
Aimar Bustamante <106636449+AimarBustamante@users.noreply.github.com>
2022-12-15 15:23:23 -0500

    Add files via upload

---
+++ js/scrollReveal-index.js
@@ -0,0 +1,8 @@
+//* Scroll Reveal
+
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.pricing', {delay: 500});
+ScrollReveal().reveal('.card', {delay: 500});
+ScrollReveal().reveal('.payment', {delay: 500});
+ScrollReveal().reveal('.form-box', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
---
+++ js/scrollReveal-seguros.js
@@ -0,0 +1,7 @@
+//* Scroll Reveal
+
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.promo__container', {delay: 500});
+ScrollReveal().reveal('.services-header', {delay: 500});
+ScrollReveal().reveal('.form-box', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
